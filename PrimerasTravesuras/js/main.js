ba193f018374a3149a3871ca8adc8c9d6087ee98
Luis Benitez <luiguibe90@hotmail.com>
2020-07-24 22:46:34 -0500

    colores diseño

--- PrimerasTravesuras/js/main.js
+++ PrimerasTravesuras/js/main.js
@@ -1,3 +1,20 @@
+ 
+ window.onscroll = function() {myFunction()};
+					
+ var navbar = document.getElementById("ftco-navbar");
+ var sticky = navbar.offsetTop;
+ 
+ function myFunction() {
+   if (window.pageYOffset >= sticky) {
+	 navbar.classList.add("sticky")
+   } else {
+	 navbar.classList.remove("sticky");
+   }
+ }
+ 
+ 
+ 
+ 
  AOS.init({
  	duration: 800,
  	easing: 'slide'
